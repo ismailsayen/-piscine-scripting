d01d055b01845a072ac78cbff79395d26d0b3cf4
isayen <ismailsvn02@gmail.com>
2026-04-04 12:24:57 +0100

    comparator.sh

--- comparator.sh
+++ comparator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 [[ -z $1 || -z $2 || -n $3 ]] && { echo The script only works with two arguments! ; exit 1 ; }
+
 [[  !(($1 =~ ^[0-9]+$)) ||  ! (($2 =~ ^[0-9]+$))   ]] && { echo Error: Only two numeric arguments are acceptable! ; exit 1 ; }
 
 if [ $1 -gt $2 ]; then
@@ -8,6 +9,5 @@ if [ $1 -gt $2 ]; then
 elif [ $1 -lt $2 ]; then
     echo "$1 < $2"
 else
-    echo "$1 = $2"
-    
+    echo "$1 = $2"  
 fi
