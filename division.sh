55dc3f08d09e6ff56fd663b55b08d42a36546a4c
isayen <ismailsvn02@gmail.com>
2026-04-04 11:33:45 +0100

    division.sh

--- division.sh
+++ division.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ $1 =~ ^[0-9]+$ && $2 =~ ^[0-9]+$ ]] ; then  
     [ $2 -eq "0" ] && { echo Error: division by zero is not allowed ; exit 0;}
-    echo "$(expr $1 / $2)"
+    echo " $1 / $2" | bc
 else
     echo Error: both arguments must be integers
     exit 0
