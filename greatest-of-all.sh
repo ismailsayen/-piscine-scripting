29ce0aae4fdb43c5694a92733e0f19e1930f6ede
isayen <ismailsvn02@gmail.com>
2026-04-04 11:52:30 +0100

    greatest-of-all.sh

--- greatest-of-all.sh
+++ greatest-of-all.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+largest=0
 
-
+for i in {1..10}
+do
 read -p "Enter a number: " n
-echo $n
-[[ ! (( $n =~ ^[0-9]+$ )) ]] && { echo no ; } 
+[[ ! (( "$n" =~ ^[0-9]+$ )) ]] && { echo ERROR: Invalid input only positive numerical characters are allowed ; exit 1 ;} 
+
+[[ "$n" -gt 10000 ]] && { echo ERROR: The number entered is too large ; exit 1 ;} 
+
+[[ "$n" -gt largest ]] && { largest=$n ;} 
+
+done
+echo The largest number is $largest
