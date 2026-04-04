a2e3fff6a681f05c41c429126bb8dc1e5eb6ed04
isayen <ismailsvn02@gmail.com>
2026-04-04 11:29:06 +0100

    jj

--- show-info.sh
+++ show-info.sh
@@ -1,5 +1,5 @@
 cat -e << inf
- "The current directory is: /home/isayen/Desktop/piscine-scripting"
- "The default paths are: /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin:/opt/flutter/bin:/usr/local/go/bin"
- "The current user is: isayen"
+The current directory is: /home/isayen/Desktop/piscine-scripting
+The default paths are: /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin:/opt/flutter/bin:/usr/local/go/bin"
+The current user is: isayen"
 inf
