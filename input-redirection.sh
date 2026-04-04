ae140e84972d19ab73a3c5d063cada0f10524b2a
isayen <ismailsvn02@gmail.com>
2026-04-04 11:29:57 +0100

    input-redirection.sh

--- input-redirection.sh
+++ input-redirection.sh
@@ -1,8 +1,8 @@
 cat << EOF > show-info.sh
 cat -e << inf
 The current directory is: $PWD
-The default paths are: $PATH"
-The current user is: $USERNAME"
+The default paths are: $PATH
+The current user is: $USERNAME
 inf
 EOF
 
