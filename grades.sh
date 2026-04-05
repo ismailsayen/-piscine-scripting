eedc21b1bf44506474ee1fe1ddfa51bb0620b9ee
isayen <ismailsvn02@gmail.com>
2026-04-05 10:38:02 +0100

    grades.sh

--- grades.sh
+++ grades.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ $# -gt 1 || -z $1 ]] && { echo "Error: expect 1 argument only!" ; exit 1 ; }
+[[ $# -gt 1 || -z $1 ]] && { echo "Error: expect 1 argument only!" >&2 ; exit 1 ; }
 
 declare -a arrr
 
@@ -8,7 +8,7 @@ for ((i = 1 ; i <= $1 ; i++));
 do
     read -p "Student Name #$i: " name
     read -p "Student Grade #$i: " grade
-    [[ ! (($grade =~ ^[0-9]+$)) || "$grade" -lt 0 || "$grade" -gt 100 ]] && { echo "Error: The grade $grade is not a valid input. Only numerical grades between 0 and 100 are accepted." ; exit 1 ;}
+    [[ ! (($grade =~ ^[0-9]+$)) || "$grade" -lt 0 || "$grade" -gt 100 ]] && { echo "Error: The grade '$grade' is not a valid input. Only numerical grades between 0 and 100 are accepted." >&2 ; exit 1 ;}
     [[ $grade -ge 90 ]] && { arrr+=("$name: You did an excellent job!") ; continue ; }
     [[ $grade -ge 70 ]] && { arrr+=("$name: You did a good job!") ; continue ; }
     [[ $grade -ge 50 ]] && { arrr+=("$name: You need a bit more effort!") ; continue ; }
