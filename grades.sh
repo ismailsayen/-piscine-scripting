14c3a98cef02439abaeff6353b7e26eb20a517d4
isayen <ismailsvn02@gmail.com>
2026-04-05 10:28:15 +0100

    grades.sh

--- grades.sh
+++ grades.sh
@@ -4,14 +4,20 @@
 
 declare -a arrr
 
-arrr+=("ssssss")
 for ((i = 1 ; i <= $1 ; i++));
 do
-    read -p "Student Name #$i:" name
-    read -p "Student Grade #$i:" grade
+    read -p "Student Name #$i: " name
+    read -p "Student Grade #$i: " grade
     [[ ! (($grade =~ ^[0-9]+$)) || "$grade" -lt 0 || "$grade" -gt 100 ]] && { echo "The grade '$grade' is not a valid input. Only numerical grades between 0 and 100 are accepted." ; exit 1 ;}
-
-
+    [[ $grade -ge 90 ]] && { arrr+=("$name: You did an excellent job!") ; continue ; }
+    [[ $grade -ge 70 ]] && { arrr+=("$name: You did a good job!") ; continue ; }
+    [[ $grade -ge 50 ]] && { arrr+=("$name: You need a bit more effort!") ; continue ; }
+    arrr+=("$name: You had a poor performance!")
 done
 
- echo "${arrr[@]}"
+# echo ${arrr[0]}
+
+for i in ${!arrr[@]}
+do 
+    echo ${arrr[$i]}
+done
