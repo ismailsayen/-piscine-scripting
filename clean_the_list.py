651d43f648a2ff512c094e0fa2a59e2a225e8aae
isayen <ismailsvn02@gmail.com>
2026-04-06 14:09:10 +0100

    clean_the_list.py

--- clean_the_list.py
+++ clean_the_list.py
@@ -1,10 +1,12 @@
 def clean_list(strs):
     if len(strs)==0:
         return []
+    for i in range(0, len(strs)):
+        strs[i]= strs[i].strip()
     if "milk" not in strs:
         strs.append("milk")
     
     for i in range(0, len(strs)):
-        strs[i]= str(1+i) + "/ " + strs[i].strip().capitalize()
+        strs[i]= str(1+i) + "/ " + strs[i].capitalize()
     
     return strs
