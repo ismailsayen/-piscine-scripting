2feaa14f605f8337f645016937e733e2152f43bd
isayen <ismailsvn02@gmail.com>
2026-04-07 16:35:27 +0100

    flex_function.py

--- flex_function.py
+++ flex_function.py
@@ -1,4 +1,6 @@
 def create_person(first_name,last_name,age=34,gender="male",size=1.83,job="taxidermist"):
+    
+    print(first_name,"-",last_name,"-",age,"-",gender,"-",size,"-",job)
     thisdict = {
         "first_name": first_name,
         "last_name": last_name,
@@ -8,3 +10,5 @@ def create_person(first_name,last_name,age=34,gender="male",size=1.83,job="taxid
         "job":job
     }
     return thisdict
+person = create_person("Jane", "Doe", size=1.75, job="programmer")
+print(person)
