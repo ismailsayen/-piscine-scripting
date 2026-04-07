b8d403c843ee66c6b8e430449a2dbd50d4f86d2d
isayen <ismailsvn02@gmail.com>
2026-04-07 14:46:45 +0100

    write_file.py

--- write_file.py
+++ write_file.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 def to_do(tasks):
     
-    file=open("output.txt","+a")
+    file=open("output.txt","w")
     for i in tasks:
        file.write(f'{i[0].strftime("%A %d %B %Y")}: {i[1]}\n')
     file.close()
