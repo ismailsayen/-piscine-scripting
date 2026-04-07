f14fc2059504e9c31e66990aa3010a60a1e580aa
isayen <ismailsvn02@gmail.com>
2026-04-07 14:43:43 +0100

    write_file.py

---
+++ write_file.py
@@ -0,0 +1,8 @@
+import datetime as dt
+
+def to_do(tasks):
+    
+    file=open("output.txt","+a")
+    for i in tasks:
+       file.write(f'{i[0].strftime("%A %d %B %Y")}: {i[1]}\n')
+    file.close()
