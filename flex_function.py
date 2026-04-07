152d470106e6f654de73e2fc8a4415d0b357e1dd
isayen <ismailsvn02@gmail.com>
2026-04-07 16:30:27 +0100

    flex_function.py

--- flex_function.py
+++ flex_function.py
@@ -8,5 +8,3 @@ def create_person(first_name,last_name,age=34,gender="male",size=1.83,job="taxid
         "job":job
     }
     return thisdict
-person = create_person("Jane", "Doe", size=1.75, job="programmer")
-print(person)
